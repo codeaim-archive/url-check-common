b2efa0b5736339e5a1d163141e31101697faf3b3
Gerard Downes <gdownes@gmail.com>
2016-02-09 14:32:56 +0000

    Readding tests

--- src/test/java/com/codeaim/urlcheck/common/model/MonitorTest.java
+++ src/test/java/com/codeaim/urlcheck/common/model/MonitorTest.java
@@ -1,24 +1,24 @@
-//package com.codeaim.urlcheck.common.model;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//public class MonitorTest
-//{
-//    @Test
-//    public void createMonitor()
-//    {
-//        Monitor newMonitor = Monitor.builder().build();
-//
-//        Assert.assertEquals(1, newMonitor.getVersion());
-//    }
-//
-//    @Test
-//    public void updateMonitor()
-//    {
-//        Monitor newMonitor = Monitor.builder().build();
-//        Monitor updatedMonitor = Monitor.buildFrom(newMonitor).build();
-//
-//        Assert.assertEquals(2, updatedMonitor.getVersion());
-//    }
-//}
+package com.codeaim.urlcheck.common.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MonitorTest
+{
+    @Test
+    public void createMonitor()
+    {
+        Monitor newMonitor = Monitor.builder().build();
+
+        Assert.assertEquals(1, newMonitor.getVersion());
+    }
+
+    @Test
+    public void updateMonitor()
+    {
+        Monitor newMonitor = Monitor.builder().build();
+        Monitor updatedMonitor = Monitor.buildFrom(newMonitor).build();
+
+        Assert.assertEquals(2, updatedMonitor.getVersion());
+    }
+}
